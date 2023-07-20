6b0355573a562ce007766547ba9acf3d5c7c5b7d
mokapot <nganhtu04597@gmail.com>
2023-07-21 03:16:45 +0700

    Make the project template

---
+++ src/factories/Ship.js
@@ -0,0 +1,18 @@
+function Ship(length) {
+  this.length = length;
+  this.hits = [];
+
+  const hit = (hitPosition) => {
+    
+  }
+
+  const isSunk = () => {
+
+  }
+
+  const getLength = () => this.length;
+
+  return {  }
+}
+
+module.exports = Ship;
